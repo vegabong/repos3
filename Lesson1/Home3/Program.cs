608d248fcb5a369c80dded3d499984442dc43606
vegabong <karpus@yandex.ru>
2021-12-17 20:06:10 +0300

    Lesson 1

---
+++ Lesson1/Home3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x1:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1:");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"{res:f}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson1/SApp02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tasks 1&2
+
+            Console.Title = "Application Form";
+            Console.Clear();
+
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Your surname:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Your age:");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Your height in meters:");
+            double height = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your weight in kg:");
+            double weight = int.Parse(Console.ReadLine());
+
+            double bmi = weight / (height*height);
+
+
+            Console.WriteLine("Name: " + name + ", surname: " + surname + ", age: " + age + ", height: " + height + ", weight: " + weight);
+            Console.WriteLine("Name: {0}, surname: {1}, age: {2}, height: {3}, weight: {4}", name, surname, age, height, weight);
+            Console.WriteLine($"Name: {name}, surname: {surname}, age: {age}, height: {height}, weight: {weight}");
+            Console.WriteLine($"Your bmi: {Math.Round(bmi, 5)}");
+
+
+            #endregion
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
